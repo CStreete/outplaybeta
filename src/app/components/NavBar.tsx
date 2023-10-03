72b3a1d9625fc9ff422e9eb86193793def35dcc7
CStreete <112933415+CStreete@users.noreply.github.com>
2023-10-03 23:22:34 +0200

    initial commit

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "@/components/ui/button"
+import { Ghost } from "lucide-react"
+
+const NavBar = () => {
+  return (
+    <nav className=" sticky h-14 inset-x-0 top-0 z-30 w-full  transition-all ">
+      <MaxWidthWrapper>
+        <div className=" flex h-14 items-center justify-between ">
+          <Link href={"/"} className=" flex z-40 font-semibold ">
+            <span className=" text-white">
+              <span className=" text-purple-400">o</span>utplay.
+            </span>
+          </Link>
+          <Link
+            href={"/"}
+            className={buttonVariants({
+              variant: "ghost",
+              size: "sm",
+              className:
+                " text-white hover:bg-gray-400/25 hover:text-white bg-",
+            })}
+          >
+            Get Notified
+          </Link>
+          <Link
+            href={"/"}
+            className={buttonVariants({
+              variant: "ghost",
+              size: "sm",
+              className: " text-white hover:bg-gray-400/25 hover:text-white",
+            })}
+          >
+            The team
+          </Link>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
+}
+export default NavBar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "./components/MaxWidthWrapper"
+import Link from "next/link"
+import Image from "next/image"
+import { ArrowRight } from "lucide-react"
+import { buttonVariants } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <>
+      <MaxWidthWrapper classname="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className=" mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-purple px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 ">
+          <p className=" text-sm font-semibold text-white">
+            Outplay is launching soon!
+          </p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl text-white">
+          <span className=" text-purple-500">O</span>utPlay
+        </h1>
+        <p className=" mt-5 max-w-prose text-white sm:text-lg">
+          A new generation of gaming, for the gamers
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <Link
+          className={buttonVariants({
+            variant: "ghost",
+            size: "lg",
+
+            className: "mt-5 bg-purple-500 text-white",
+          })}
+          href={"/prelaunch"}
+        >
+          Get Notified
+          <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+      <div>
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className=" mt-16 flex justify-center sm:mt-24 ">
+            <div className="-m-2 rounded-xl bg-purple-950/10 px-8 ">
+              <Image
+                src={"/Wireframe4.png"}
+                height={1364}
+                width={866}
+                quality={100}
+                alt="ui image"
+              ></Image>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-white sm:text-5xl">
+              A gaming platform created for the true gamer
+            </h2>
+            <p className="mt-4 text-lg text-white">
+              Sharing you favorite clips has never been easier than with
+              Outplay.
+            </p>
+          </div>
+        </div>
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-xl font-semibold text-purple-300">
+                Interact with Communities!
+              </span>
+              <span className="mt-2 text-white">
+                Discover a world of possibilities with our app designed to help
+                you explore diverse content and engage with vibrant communities!
+                Whether you're a content creator, an explorer, or a social
+                butterfly, we've got you covered.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-xl font-semibold text-purple-300">
+                Post and share your favorite clips!
+              </span>
+              <span className="mt-2 text-white">
+                Introducing our cutting-edge app that empowers you to share your
+                favorite clips like never before! Whether it's a hilarious
+                moment, an epic win, or a heartwarming scene, Post and Share
+                Your Favorite Clips to showcase the best moments.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-xl font-semibold text-purple-300">
+                Build a community!
+              </span>
+              <span className="mt-2 text-white">
+                Welcome to the ultimate community-building app where you're in
+                control! With "Build a Community," creating, nurturing, and
+                connecting with your tribe has never been easier.
+              </span>
+            </div>
+          </li>
+        </ol>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
