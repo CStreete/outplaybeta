9a79500a2c3baf083bc308c685da3061b0912458
CStreete <112933415+CStreete@users.noreply.github.com>
2023-10-04 12:35:02 +0200

    added db and sumbit

---
+++ src/app/api/subscribe/route.ts
@@ -0,0 +1,22 @@
+import db from "@/lib/db"
+import { NextApiRequest, NextApiResponse } from "next"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+  if (req.method !== "POST") {
+    return new NextResponse("Invalid Request", { status: 405 })
+  }
+  try {
+    const { email } = await req.json()
+
+    const newSubscription = await db.users.create({
+      data: {
+        email,
+      },
+    })
+    return NextResponse.json(newSubscription)
+  } catch (error) {
+    console.log("[COURSES]", error)
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -1,17 +1,34 @@
+"use client"
 import { NextPage } from "next"
 import MaxWidthWrapper from "../components/MaxWidthWrapper"
 import { Input } from "@/components/ui/input"
-
-import { ArrowDown } from "lucide-react"
+import axios from "axios"
 import { Button } from "@/components/ui/button"
+import { SyntheticEvent, useState } from "react"
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
+  const [formData, setFormData] = useState({
+    email: "",
+  })
+
+  const submitRegistration = async (e: SyntheticEvent) => {
+    e.preventDefault()
+    try {
+      await axios.post("/api/subscribe", {
+        email: formData.email,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <MaxWidthWrapper classname="mb-12 mt-28 sm:mt-40 flex flex-col items-start justify-center text-center">
       <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl text-white">
         <span className=" text-purple-500">O</span>utPlay IS COMING SOON
+        <span></span>
       </h1>
       <p className="mt-5 font-semibold max-w-prose text-white sm:text-2xl">
         Be one of the first users to experince outplay once it has launched!
@@ -19,11 +36,18 @@ const Page: NextPage<Props> = ({}) => {
 
       <div className="mt-28 w-full sm:flex sm:items-center">
         <p className=" text-white ">Enter your email to get notified!</p>
-        <div className=" relative w-full flex space-x-2 items-center sm:w-1/2">
+
+        <form
+          onSubmit={submitRegistration}
+          className="relative w-full flex space-x-2 items-center sm:w-1/2"
+        >
           <Input
             type="email"
             placeholder="Email"
             className="rounded-full ml-6 w-full  placeholder:text-white text-white py-6"
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
           ></Input>
           <Button
             variant={"ghost"}
@@ -33,7 +57,7 @@ const Page: NextPage<Props> = ({}) => {
           >
             Subscribe
           </Button>
-        </div>
+        </form>
       </div>
     </MaxWidthWrapper>
   )
