a225688d798e005b4f6a08f2f974fa08c3ddf87d
CStreete <112933415+CStreete@users.noreply.github.com>
2023-10-04 13:23:55 +0200

    added message for subscribe

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -3,12 +3,15 @@ import { NextPage } from "next"
 import MaxWidthWrapper from "../components/MaxWidthWrapper"
 import { Input } from "@/components/ui/input"
 import axios from "axios"
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { SyntheticEvent, useState } from "react"
+import Link from "next/link"
+import { ArrowRight } from "lucide-react"
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
+  const [action, setAction] = useState("")
   const [formData, setFormData] = useState({
     email: "",
   })
@@ -19,6 +22,7 @@ const Page: NextPage<Props> = ({}) => {
       await axios.post("/api/subscribe", {
         email: formData.email,
       })
+      setAction("sent")
     } catch (error) {
       console.log(error)
     }
@@ -35,29 +39,52 @@ const Page: NextPage<Props> = ({}) => {
       </p>
 
       <div className="mt-28 w-full sm:flex sm:items-center">
-        <p className=" text-white ">Enter your email to get notified!</p>
+        {action == "sent" ? (
+          <div>
+            <h1 className=" text-2xl text-white">
+              Thank You for your intrest in outplay, we will email u as soon as
+              possible with future updates!
+            </h1>
+            <Link
+              className={buttonVariants({
+                variant: "ghost",
+                size: "lg",
 
-        <form
-          onSubmit={submitRegistration}
-          className="relative w-full flex space-x-2 items-center sm:w-1/2"
-        >
-          <Input
-            type="email"
-            placeholder="Email"
-            className="rounded-full ml-6 w-full  placeholder:text-white text-white py-6"
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
-          ></Input>
-          <Button
-            variant={"ghost"}
-            size={"lg"}
-            type="submit"
-            className="bg-purple-500 text-whit absolute right-0 rounded-full text-white py-7"
-          >
-            Subscribe
-          </Button>
-        </form>
+                className: "mt-5 bg-purple-500 text-white",
+              })}
+              href={"/"}
+            >
+              Return
+              <ArrowRight className="ml-2 h-5 w-5" />
+            </Link>
+          </div>
+        ) : (
+          <>
+            <p className=" text-white ">Enter your email to get notified!</p>
+
+            <form
+              onSubmit={submitRegistration}
+              className="relative w-full flex space-x-2 items-center sm:w-1/2"
+            >
+              <Input
+                type="email"
+                placeholder="Email"
+                className="rounded-full ml-6 w-full  placeholder:text-white text-white py-6"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              ></Input>
+              <Button
+                variant={"ghost"}
+                size={"lg"}
+                type="submit"
+                className="bg-purple-500 text-whit absolute right-0 rounded-full text-white py-7"
+              >
+                Subscribe
+              </Button>
+            </form>
+          </>
+        )}
       </div>
     </MaxWidthWrapper>
   )
